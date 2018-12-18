9422d76bbc6d372e7b20dfba2cb0b569674f54e8
unknown <ymbrdii2002@gmail.com>
2018-12-18 09:38:29 +0900

    First commit

--- day01/money.py
+++ day01/money.py
@@ -4,5 +4,7 @@ import bs4
 response = requests.get('https://finance.naver.com/marketindex/').text
 soup = bs4.BeautifulSoup(response, 'html.parser' )
 result = soup.select_one('#exchangeList > li.on > a.head.usd > div > span.value').text
-
+a = '추가.'
+print('지금 원/달러 환율은' + result + '입니다.')
 print(f'지금 원/달러 환율은 {result} 입니다. ')
+print('지금 원/달러 환율은 {} 입니다. {}'.format(result,a))
--- day01/req.py
+++ day01/req.py
@@ -3,6 +3,6 @@ import bs4
 
 response = requests.get('https://finance.naver.com/marketindex/?tabSel=exchange#tab_section').text
 soup = bs4.BeautifulSoup(response, 'html.parser' )
-result = soup.select_one('#blind').text
+result = soup.select_one('#exchangeList > li.on > a.head.usd > div > span.value').text
 print(result)
 #ctrl + '(backtick)
