f74f36e128906e944f78586c2450b19fc82c24ab
Gyujin Lee <ryanlee5646@gmail.com>
2019-01-15 17:46:41 +0900

    D10 | 190115 PM | CSS

--- Problems/SWEA/6244.py
+++ Problems/SWEA/6244.py
@@ -3,10 +3,21 @@
 # while 문과 리스트 객체의 pop()을 이용해 80점 이상의 점수들의 총합을 구하시오.
 score = [85, 65, 77, 83, 75, 22, 98, 88, 38, 100]
 count = 0
-sum_score = 0
-while count < 10:  
-    if score[-1] < 80:
-        score.pop() 
+sum_score =0
+while count < 10: 
+    i = score.pop(0) 
     count += 1
+    if i >= 80:
+        sum_score += i     
 print(sum_score)
     
+# l= [85, 65, 77, 83, 75, 22, 98, 88, 38, 100]
+# sum_i = 0
+
+# count =0
+# while count != 10:
+#     i = l.pop(0)
+#     count += 1
+#     if i >= 80:
+#         sum_i += i
+# print(sum_i)
