ef8637d1220db385ce3e55f3eb02af0de65d3a06
Gyujin Lee <ryanlee5646@gmail.com>
2019-03-14 18:00:40 +0900

    | 190314 | algorithm

--- algorithm/D18/Sudoku.py
+++ algorithm/D18/Sudoku.py
@@ -2,24 +2,47 @@ import sys
 sys.stdin = open("Sudoku.txt","r")
 
 
-T = int(input())
-for t in range(1,T+1):
-    data = []
-    for i in range(9):
-        data.append(list(map(int,input().split())))
-
+def garo():
+    global flag
     for y in range(9):
-        visited_x = [1]+[0]*9
-        visited_y = [1]+[0]*9
-        flag = True
+        count = [1] + [0] * 9
         for x in range(9):
-            visited_x[data[y][x]] = 1
-            visited_y[data[x][y]] = 1
-            for z in range(1,10):
-                if(visited_x[z] != 1) or (visited_y[z]!= 1):
-                    flag = False
-                    break
+            count[data[y][x]] = 1
+        if 0 in count:
+            flag = False
+            return
+def sero():
+    global flag
+    for x in range(9):
+        count = [1] + [0] * 9
+        for y in range(9):
+            count[data[y][x]] = 1
+        if 0 in count:
+            flag = False
+            return
+def square():
+    global flag
+    for y in range(0, 9, 3):
+        for x in range(0, 9, 3):
+            count = [1] + [0] * 9
+            for i in range(3):
+                for j in range(3):
+                    count[data[y + i][x + j]] = 1
+            if 0 in count:
+                flag = False
+                return
+
+T = int(input())
+for t in range(1, T + 1):
+    data = []
+    flag = True
+    for i in range(9):
+        data.append(list(map(int, input().split())))
+    garo()
+    sero()
+    square()
+
     if flag:
-        print("#{} 1".format(t))
+        print("#{} 1 ".format(t))
     else:
         print("#{} 0".format(t))
