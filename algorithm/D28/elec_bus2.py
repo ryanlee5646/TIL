103e325a240e4097a0a4dbe92ae58535f43b9487
Gyujin Lee <ryanlee5646@gmail.com>
2019-03-27 18:03:26 +0900

    | 190327 | algo

--- algorithm/D28/elec_bus2.py
+++ algorithm/D28/elec_bus2.py
@@ -1,6 +1,13 @@
 import sys
 sys.stdin = open("elec_bus2.txt","r")
 
+def Back(here,):
+    global count
+
+
+
+
+
 T = int(input())
 for t in range(1,T+1):
     charge = list(map(int,input().split()))
@@ -11,9 +18,5 @@ for t in range(1,T+1):
     visited = [0]*(N-1)
     print(visited)
     charge_num = 0
-    while True:
-        for i in range(charge):
-            if not visited[i]:
-                visited[i] = 1
-                charge_num = charge[i]
-                
+
+
