3d3ebf34ea271c703173b7e696cbad1713675f88
Gyujin Lee <ryanlee5646@gmail.com>
2019-04-04 18:02:10 +0900

    190404

--- algorithm/D31/sangwon_party.py
+++ algorithm/D31/sangwon_party.py
@@ -1,43 +1,38 @@
-import sys
+import sys,time
 sys.stdin = open("sangwon_party.txt", "r")
+stime = time.time()
 
-T = int(input())
 
 def BFS(start):
     queue = [start]
     visited[start] = 1
+
     while queue:
         y = queue.pop(0)
         for x in range(1,N+1):
             if mymap[y][x] == 1 and not visited[x]:
                 visited[x] = visited[y] + 1
                 queue.append(x)
-        # if visited[x] > 3:
-        #     return
 
+
+T = int(input())
 for t in range(1,T+1):
     N, M = map(int, input().split())
     data = [list(map(int,input().split())) for i in range(M)]
     count = 0
-    # print(data)
     mymap = [[0]*(N+1) for i in range(N+1)]
     start = []
     visited = [0]*(N+1)
-    # print(visited)
     for i in data:
         mymap[i[0]][i[1]] = 1
         mymap[i[1]][i[0]] = 1
-        # if i[0] == 1:
-        #     start.append(i)
-        #     visited[i[0]][i[1]] = 1
-    # print(mymap)
     BFS(1)
-    # print(visited)
+    print(visited)
     for i in visited:
-        if 0 < i <=3:
+        if 1 < i <=3:
             count+=1
-
     if count == 0:
         print("#{} 0".format(t))
     else:
-        print("#{} {}".format(t,count-1))
+        print("#{} {}".format(t,count))
+    print(time.time()-stime)
