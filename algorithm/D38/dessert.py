94f82709e917cf8b30efe5126ca42d57bfdaf618
Gyujin Lee <ryanlee5646@gmail.com>
2019-04-30 18:02:33 +0900

    190430

--- algorithm/D38/dessert.py
+++ algorithm/D38/dessert.py
@@ -2,17 +2,43 @@ import sys
 sys.stdin = open('dessert.txt','r')
 
 def IsSafe(y,x):
-    if 0<=y<N and 0<=x<N:
+    if 0<=y<N and 0<=x<N and visited[data[y][x]] != 1:
         return True
     else:
         return False
 
-dy = [1, 1,-1,-1]
-dx = [-1, 1,1,-1]
+def DFS(y, x, count):
+    global result
+    if y == sy and x == sx and count > 3 :
+        if count > result:
+            result = count
+            return
+
+    for i in range(4):
+        ny = y+dy[i]
+        nx = x+dx[i]
+        if IsSafe(ny,nx):
+            visited[data[ny][nx]] = 1
+            DFS(ny,nx, count+1)
+            visited[data[ny][nx]] = 0
+
+
+dy = [1, 1,-1,-1] # 좌, 우, 상, 상
+dx = [-1, 1,1,-1] # 하, 하, 우, 좌
 
 T = int(input())
 for t in range(1,T+1):
     N = int(input())
     data = [list(map(int,input().split())) for _ in range(N)]
-    print(data)
-
+    visited = [0] * 101
+    result = 0
+    for y in range(N):
+        for x in range(N):
+            sy = y
+            sx = x
+            DFS(y,x,0)
+            visited = [0] * 101
+    if result < 4:
+        print("#{} -1".format(t))
+    else:
+        print("#{} {}".format(t, result))
