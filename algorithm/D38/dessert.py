8e888f7e4be94289eaa1c3019e5a0c32dd9e9bee
Gyujin Lee <ryanlee5646@gmail.com>
2019-05-07 09:06:24 +0900

    190506

--- algorithm/D38/dessert.py
+++ algorithm/D38/dessert.py
@@ -7,24 +7,27 @@ def IsSafe(y,x):
     else:
         return False
 
-def DFS(y, x, count):
+def DFS(y, x, count, dir):
     global result
-    if y == sy and x == sx and count > 3 :
+    if y == sy and x == sx and dir == 3 :
         if count > result:
             result = count
             return
+    ny = y + dy[dir]
+    nx = x + dx[dir]
+    if IsSafe(ny, nx):
+        visited[data[ny][nx]] = 1
+        DFS(ny,nx,count+1,dir)
+        visited[data[ny][nx]] = 0
 
-    for i in range(4):
-        ny = y+dy[i]
-        nx = x+dx[i]
-        if IsSafe(ny,nx):
-            visited[data[ny][nx]] = 1
-            DFS(ny,nx, count+1)
-            visited[data[ny][nx]] = 0
+    if dir<3 and IsSafe(y+dy[dir+1], x+dx[dir+1]):
+        visited[data[y+dy[dir+1]][x+dx[dir+1]]] = 1
+        DFS(y+dy[dir+1],x+dx[dir+1],count+1,dir+1)
+        visited[data[y+dy[dir+1]][x+dx[dir+1]]] = 0
 
 
-dy = [1, 1,-1,-1] # 좌, 우, 상, 상
-dx = [-1, 1,1,-1] # 하, 하, 우, 좌
+dy = [1, 1,-1,-1] # 하, 하, 상, 상
+dx = [-1, 1,1,-1] # 좌, 우, 우, 좌
 
 T = int(input())
 for t in range(1,T+1):
@@ -32,12 +35,10 @@ for t in range(1,T+1):
     data = [list(map(int,input().split())) for _ in range(N)]
     visited = [0] * 101
     result = 0
-    for y in range(N):
-        for x in range(N):
-            sy = y
-            sx = x
-            DFS(y,x,0)
-            visited = [0] * 101
+    for sy in range(N):
+        for sx in range(N):
+            DFS(sy,sx,0, 0)
+
     if result < 4:
         print("#{} -1".format(t))
     else:
