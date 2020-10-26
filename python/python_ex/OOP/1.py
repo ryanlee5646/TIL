7f2df3460d3c2c0e142586ecc94b8cb7243aa047
ryanlee <ryanlee5646@gmail.com>
2020-10-27 00:58:01 +0900

    |20201027 python OOP TIL|

---
+++ python/python_ex/OOP/1.py
@@ -0,0 +1,30 @@
+class Cal(object):
+    def __init__(self, v1, v2):
+        self.v1 = v1
+        self.v2 = v2
+    
+    def add(self):
+
+        return self.v1 + self.v2
+
+    def subtract(self):
+
+        return self.v1 - self.v2
+
+
+
+
+
+c1 = Cal(10,10)
+
+# 덧셈
+print(c1. add())
+
+# 빼기
+print(c1.subtract())
+
+
+c2 = Cal(30,20)
+
+print(c2.add())
+print(c2.subtract())
